2c637c14fbf96d4dba49f57751b371d3ad1d6dae
EvaTu <evatuw@gmail.com>
2021-07-14 01:47:33 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solutions in this file
+/*for (let age=30;age<40;age++){
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    debugger;
+}
+*/
+
+/*const gifts = ["teddy bear","drone","doll"];
+
+function wrapGifts(gifts){
+    for (let i=0;i<gifts.length;i++){
+        console.log(`Wrapped ${gifts[i]} and add a bow!`);
+        debugger;
+    }
+    return gifts;
+}
+wrapGifts(gifts);
+*/
+
+function writeCards(namesArray,event){
+    let thankYouCards = []
+    for(let i=0;i<namesArray.length;i++){
+        thankYouCards.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`);
+    }
+return thankYouCards
+}
+
+
+function countDown(){
+    let count = 10;
+    while (count>=0){
+    console.log(count);
+    count = count-1;
+    }
+}
